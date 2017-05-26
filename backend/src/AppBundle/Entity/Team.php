f235a77727a8b13f858d3c7607fd1bd1e5d2a4ba
Emiliano Viada <emjovi@gmail.com>
2017-05-26 00:26:35 -0300

    Added Team entity and admin module

---
+++ backend/src/AppBundle/Entity/Team.php
@@ -0,0 +1,229 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+
+/**
+ * Team
+ *
+ * @ORM\Table(name="teams")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TeamRepository")
+ * @Vich\Uploadable
+ */
+class Team
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alias", type="string", length=255, nullable=true)
+     */
+    private $alias;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="flag", type="string", length=255, nullable=true)
+     */
+    private $flag;
+
+    /**
+     * @Vich\UploadableField(mapping="team_images", fileNameProperty="flag")
+     * @var File
+     */
+    private $flagFile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="stadium", type="string", length=255, nullable=true)
+     */
+    private $stadium;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Team
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set alias
+     *
+     * @param string $alias
+     *
+     * @return Team
+     */
+    public function setAlias($alias)
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    /**
+     * Get alias
+     *
+     * @return string
+     */
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    /**
+     * Set flag
+     *
+     * @param string $flag
+     *
+     * @return Team
+     */
+    public function setFlag($flag)
+    {
+        $this->flag = $flag;
+
+        return $this;
+    }
+
+    /**
+     * Get flag
+     *
+     * @return string
+     */
+    public function getFlag()
+    {
+        return $this->flag;
+    }
+
+    /**
+     * Set stadium
+     *
+     * @param string $stadium
+     *
+     * @return Team
+     */
+    public function setStadium($stadium)
+    {
+        $this->stadium = $stadium;
+
+        return $this;
+    }
+
+    /**
+     * Get stadium
+     *
+     * @return string
+     */
+    public function getStadium()
+    {
+        return $this->stadium;
+    }
+
+    /**
+     * setFlagFile
+     * @param string $flag
+     */
+    public function setFlagFile(File $flag = null)
+    {
+        $this->flagFile = $flag;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($flag) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    /**
+     * getFlagFile() getter
+     */
+    public function getFlagFile()
+    {
+        return $this->flagFile;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Team
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
