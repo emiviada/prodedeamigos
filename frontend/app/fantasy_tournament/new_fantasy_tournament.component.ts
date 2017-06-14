702dfee98ca98d4bd8726aad8bbe75c2020b13fa
Emiliano Viada <emjovi@gmail.com>
2017-06-13 23:44:57 -0300

    Added UI functionality for create a new fantasy tournament

--- frontend/app/fantasy_tournament/new_fantasy_tournament.component.ts
+++ frontend/app/fantasy_tournament/new_fantasy_tournament.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { FantasyTournament } from '../model/fantasyTournament';
 
 @Component({
   templateUrl: './new_fantasy_tournament.component.html'
 })
 export class NewFantasyTournamentComponent {
-    constructor() { }
+
+    tournaments = ['Torneo 1', 'Torneo 2'];
+    pointsPerGameOptions = [2, 3, 4, 5];
+    pointsPerExactOptions = [1, 2];
+    model = new FantasyTournament;
+    submitted = false;
+
+    constructor(private location: Location) {
+        this.model.pointsPerGame = 3;
+    }
+
+    upsert(): void { // Create or Edit
+        alert(this.submitted);
+        console.log(JSON.stringify(this.model));
+        this.model = new FantasyTournament;
+    }
+
+    onSubmit() { this.submitted = true; }
+
+    cancel(): void {
+        this.location.back();
+    }
 }
--- frontend/app/model/fantasyTournament.ts
+++ frontend/app/model/fantasyTournament.ts
@@ -1,4 +1,8 @@
 export class FantasyTournament {
   id: number;
   name: string;
+  tournament: string;
+  pointsPerGame: number;
+  matchExact: boolean;
+  pointsPerExact: number;
 }
